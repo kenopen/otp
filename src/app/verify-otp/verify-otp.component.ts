da70d34a3c0974dc7df4e7513d1b530853094eba
Kenneth <kenneth@blane.co.za>
2023-12-03 04:59:32 +0000

    add verification component

---
+++ src/app/verify-otp/verify-otp.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-verify-otp',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './verify-otp.component.html',
+  styleUrl: './verify-otp.component.css'
+})
+export class VerifyOtpComponent {
+
+}
