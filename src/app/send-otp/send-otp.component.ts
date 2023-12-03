20401525f56caba74015f4999307e332cb7ee975
Kenneth <kenneth@blane.co.za>
2023-12-03 04:47:57 +0000

    add send otp component

---
+++ src/app/send-otp/send-otp.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-send-otp',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './send-otp.component.html',
+  styleUrl: './send-otp.component.css'
+})
+export class SendOtpComponent {
+
+}
