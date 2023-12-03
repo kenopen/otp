6dbece496a111fad05192cdf75127350cd77898c
Kenneth <kenneth@blane.co.za>
2023-12-03 11:40:56 +0000

    add routing for send otp component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { SendOtpComponent } from './send-otp/send-otp.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: SendOtpComponent },
+    //TODO: Add route for verify otp component
+];
